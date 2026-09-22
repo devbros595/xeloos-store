4bce1db80b842e739261ce77d9ff285e6e4e9781
devbros595 <atejiri@outlook.com>
2026-09-22 01:53:00 +0100

    update index

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,10 +1,12 @@
 from .cart import Cart
 
-# Create context processor so cart works on all page
+
 def cart(request):
     cart_instance = Cart(request)
+
     return {
-        'cart': cart_instance,
-        'cart_items': cart_instance.get_sims(),
-        "cart_total": cart_instance.get_total_price()
+        "cart": cart_instance,
+        "cart_items": cart_instance.get_items(),
+        "cart_total": cart_instance.get_total_price(),
+        "cart_count": len(cart_instance),
     }
